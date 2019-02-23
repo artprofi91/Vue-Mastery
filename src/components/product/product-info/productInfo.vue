3d95afe82550849d485ed61f3c870d14a6e45f59
Tregubchak, Artem <artem.tregubchak@accenture.com>
2019-02-23 16:05:28 -0600

    list rendering

--- src/components/product/product-info/productInfo.vue
+++ src/components/product/product-info/productInfo.vue
@@ -4,6 +4,19 @@
     <p v-if="inStock">In Stock</p>
     <p v-if="!inStock">Out of Stock</p>
     <span v-if="onSale && inStock">On Sale!</span>
+    <ul>
+      <li v-for="detail in details" :key="detail.id">
+        {{ detail.desc }}
+      </li>
+    </ul>
+    <div v-for="variant in variants" :key="variant.variantId">
+      <p>{{ variant.variantColor }}</p>
+    </div>
+    <ul>
+      <li v-for="size in sizes" :key="size.id">
+        {{ size.size }}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -14,7 +27,23 @@ export default {
     return {
       product: "Socks",
       inStock: false,
-      onSale: true
+      onSale: true,
+      details: [
+        { desc: "80% cotton", id: 1 },
+        { desc: "20% polyester", id: 2 },
+        { desc: "Gender-neutral", id: 3 }
+      ],
+      variants: [
+        { variantId: 111, variantColor: "green" },
+        { variantId: 112, variantColor: "blue" }
+      ],
+      sizes: [
+        { size: "S", id: 10 },
+        { size: "M", id: 11 },
+        { size: "L", id: 12 },
+        { size: "XL", id: 13 },
+        { size: "XXL", id: 14 }
+      ]
     };
   }
 };
