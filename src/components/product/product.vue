503a8c57e050b5a9210d39db8c5d6940a426d539
Tregubchak, Artem <artem.tregubchak@accenture.com>
2019-02-24 13:54:24 -0600

    froms validation and error handeling

--- src/components/product/product.vue
+++ src/components/product/product.vue
@@ -5,19 +5,33 @@
       @colorHover="onColorHover"
       @addToCart="onAddToCart"
     ></ProductInfo>
+    <div>
+      <h2>Reviews</h2>
+      <p v-if="!reviews.length">There are no reviews yet.</p>
+      <ul>
+        <li v-for="(review, index) in reviews" :key="index">
+          <p>{{ review.name }}</p>
+          <p>Rating: {{ review.rating }}</p>
+          <p>{{ review.review }}</p>
+        </li>
+      </ul>
+    </div>
+    <ProductReview @reviewSubmitted="onReviewSubmitted" />
   </div>
 </template>
 
 <script>
 import ProductImage from "./product-image/productImage.vue";
 import ProductInfo from "./product-info/productInfo.vue";
+import ProductReview from "./product-review/productReview.vue";
 
 export default {
   name: "Product",
-  components: { ProductImage, ProductInfo },
+  components: { ProductImage, ProductInfo, ProductReview },
   data() {
     return {
-      imagePath: require("@/assets/green.png")
+      imagePath: require("@/assets/green.png"),
+      reviews: []
     };
   },
   methods: {
@@ -26,6 +40,9 @@ export default {
     },
     onAddToCart(event) {
       this.$emit("addToCart", event);
+    },
+    onReviewSubmitted(productReview) {
+      this.reviews.push(productReview);
     }
   }
 };
